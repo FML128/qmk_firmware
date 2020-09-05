05f30acb0355124333d6c163c365a98e7233834c
Merlin Jehli <merlin@jeh.li>
2020-09-06 00:18:48 +0200

    First implementation FML128 user and layout

---
+++ users/fml128/rules.mk
@@ -0,0 +1,14 @@
+SRC += fml128.c
+
+# Build Options
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no           # USB Nkey Rollover
+MIDI_ENABLE = no            # MIDI support
+UNICODE_ENABLE = no         # Unicode
+VIRTSER_ENABLE = no         # USB Serial Driver
+RAW_ENABLE = no             # Raw device
+AUTO_SHIFT_ENABLE = no      # Auto Shift
