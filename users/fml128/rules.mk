552519a5126991e5f5982af9f9af51d0c4db85a4
Merlin Jehli <merlin@jeh.li>
2020-09-06 23:14:13 +0200

    Updated rules

--- users/fml128/rules.mk
+++ users/fml128/rules.mk
@@ -1,12 +1,14 @@
 SRC += fml128.c
 
 # Build Options
+LTO_ENABLE = yes            # Enables Link Time Optimization
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
+EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no           # USB Nkey Rollover
+NKRO_ENABLE = no            # USB Nkey Rollover
+LEADER_ENABLE = no          # Enable leader key chording
 MIDI_ENABLE = no            # MIDI support
 UNICODE_ENABLE = no         # Unicode
 VIRTSER_ENABLE = no         # USB Serial Driver
