30388275b08ede6eaeebffc2e009ed2e3d6a2310
Merlin Jehli <merlin@jeh.li>
2021-04-08 09:45:39 +0200

    light timeout

--- keyboards/massdrop/alt/keymaps/fml128/keymap.c
+++ keyboards/massdrop/alt/keymaps/fml128/keymap.c
@@ -1,10 +1,15 @@
 #include QMK_KEYBOARD_H
 
+#ifdef RGB_MATRIX_ENABLE
+    static uint32_t idle_timer;
+    static bool led_on = true;
+#endif
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
-        RGB_TOG, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  _______, \
+        _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  _______, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   _______, \
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
@@ -13,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______,_______,_______, _______, _______, _______, _______, _______, \
         RESET, _______,_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PGUP, _______, \
-        _______, _______, _______,                            _______,                            _______, _______, _______, KC_PGDN, _______  \
+        KC_RCTL, _______, _______,                            _______,                            _______, _______, _______, KC_PGDN, _______  \
     ),
     /*
     [X] = LAYOUT(
@@ -27,5 +32,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 void matrix_init_user(void) {
-  rgblight_sethsv(HSV_GREEN);
+    #ifdef RGB_MATRIX_ENABLE
+        rgblight_sethsv(COLOR);
+    #endif
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        #ifdef RGB_MATRIX_ENABLE
+            if (led_on == false) {
+                rgblight_enable_noeeprom();
+                led_on = true;
+            }
+        idle_timer = timer_read();
+        #endif
+    }
+    return true;
+  }
+
+void matrix_scan_user(void) {
+    #ifdef RGB_MATRIX_ENABLE
+        if ( led_on && timer_elapsed(idle_timer) > BACKLIGHT_TIMEOUT * 60000) {
+            rgblight_disable_noeeprom();
+            led_on = false;
+        }
+    #endif
+}
